93d80f917fd13c4b1ffba1396d3cbf8c46830742
tanjina-hub <67520120+tanjina-hub@users.noreply.github.com>
2021-04-06 00:13:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 function App() {
-  return (
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+     // fires when app component mounts to the DOM
+   const storageTodos = Json.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+   if (storageTodos) {
+     setTodos(storageTodos);
+   }
+    }
+  }, []);
+
+  useEffect(() => {
+    // fires when todos array gets updated
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+    }
+  }, []);
+
+  function addTodo(todo) {
+    // adds new todo to beginning of todos array
+    setTodos([todo, ...todos]);
+  }
+
+
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography style={{ padding: 16 }} variant="h1">
+        React Todo
+      </Typography>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        
+      />
     </div>
   );
 }
